04f479b7f8e09248a5898e0594981194ccb64e31
Marioz18 <zelayamarwert12@gmail.com>
2022-04-03 22:46:06 -0600

    inicio web administrador

---
+++ administrador/js/controlador.js
@@ -0,0 +1,24 @@
+var empresas=[]
+
+var productos=[]
+
+var motoristas=[]
+
+var pedidos=[]
+
+var perfiles=[]
+
+var perfilActual=[]
+
+
+
+
+
+
+
+
+
+function mostrarFondo1(){
+    document.getElementById('fondo').classList.add('fondoIniciarS');
+}
+mostrarFondo1();
